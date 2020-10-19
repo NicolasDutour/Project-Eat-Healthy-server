2483e2b66a871fcb13ff12050535f1400a2adda5
NicolasDutour <dutourn99@gmail.com>
2020-10-19 20:56:35 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '91951cd92d90390820da1bcb54dd14e2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5c95b094-180c-46d7-bad2-b91c8f19e58f'
+};
